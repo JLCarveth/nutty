5aa5bef9b5542f09fbab419dcac64ae4f41980d9
John Carveth <jlcarveth@gmail.com>
2023-03-14 16:07:16 -0400

    Example Paste server implementation

---
+++ paste.ts
@@ -0,0 +1,45 @@
+/**
+ * A Pastebin-like backend using Zippy
+ *
+ * @author John L. Carveth <jlcarveth@gmail.com>
+ * @version 0.1.0
+ *
+ * 2023-03-10
+ */
+import { get, listen, post } from "./server.ts";
+import { serveFile } from "https://deno.land/std@0.179.0/http/file_server.ts";
+
+const PORT = Number.parseInt(<string> Deno.env.get("PORT") ?? 5335);
+const SECRET_KEY = Deno.env.get("SECRET_KEY") || "__NOKEY__";
+const TARGET_DIR = Deno.env.get("TARGET_DIR") || "/opt/paste/";
+
+/**
+ * POST /api/paste
+ * Expects the SECRET_KEY to be provided via X-Access-Token header.
+ * Creates a new file on the filesystem, writes the POST body to the file,
+ * and returns a UUID filename on success.
+ */
+post("/api/paste", async (req, _path, _params) => {
+  const filename = crypto.randomUUID();
+  const token = req.headers.get("X-Access-Token");
+  const text = await req.text();
+  if (!token || token !== SECRET_KEY) {
+    return new Response(
+      "Missing or invalid secret key..." + token,
+      {
+        status: 401,
+      },
+    );
+  }
+  const encoder = new TextEncoder();
+  const data = encoder.encode(text);
+  await Deno.writeFile(`${TARGET_DIR}/${filename}`, data);
+  return new Response(filename);
+});
+
+get("/api/:uuid", (req, _path, params) => {
+  const filename = params?.uuid;
+  return serveFile(req, TARGET_DIR + "/" + filename);
+});
+
+listen(PORT);
--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ const routes: Route[] = [];
 const DEBUG = Deno.env.get("DEBUG") ?? false;
 /**
  * Handles and incoming HTTP request
- * TODO Pass off route handling to a Web Worker
  */
 async function serveHttp(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
@@ -17,10 +16,14 @@ async function serveHttp(conn: Deno.Conn) {
     for (const route of routes) {
       if (route.action === method && route.path.test(path)) {
         if (DEBUG) console.log(JSON.stringify({ action: method, path: path }));
-        // Use the routes handler to generate a Response object
         const match = route.path.exec(requestEvent.request.url);
         const params = match?.pathname.groups;
-        const response = route.handler(requestEvent.request, route.path, params);
+        /* Pass off the request info, params to the registered handler */
+        const response = route.handler(
+          requestEvent.request,
+          route.path,
+          params,
+        );
         requestEvent.respondWith(response);
         continue;
       }
@@ -28,19 +31,33 @@ async function serveHttp(conn: Deno.Conn) {
   }
 }
 
-type RouteHandler = (req : Request, path : URLPattern, params? : Record<string, unknown>) => Response;
+type RouteHandler = (
+  req: Request,
+  path: URLPattern,
+  params?: Record<string, unknown>,
+) => Promise<Response>;
 
-export function get(path : string, handler: RouteHandler) {
-  const urlPattern = new URLPattern({ 'pathname' : path });
-  routes.push({'path' : urlPattern, 'action' : 'GET', handler: handler});
+export function get(path: string, handler: RouteHandler) {
+  const urlPattern = new URLPattern({ pathname: path });
+  routes.push({ path: urlPattern, action: "GET", handler: handler });
 }
 
+export function post(path: string, handler: RouteHandler) {
+  const urlPattern = new URLPattern({ pathname: path });
+  routes.push({ path: urlPattern, action: "POST", handler: handler });
+}
+
+/** */
+export function addRoute(path: string, action: string, handler: RouteHandler) {
+  const urlPattern = new URLPattern({ pathname: path });
+  routes.push({ path: urlPattern, action: action, handler: handler });
+}
 /**
  * Call this function to start the server.
  * @param port The port the server will listen to
  */
-export async function listen(port : number) {
-  const listener = Deno.listen({'port' : port});
+export async function listen(port: number) {
+  const listener = Deno.listen({ port: port });
   if (DEBUG) console.log(`Now listening on localhost:${port}/`);
   for await (const conn of listener) {
     serveHttp(conn);
