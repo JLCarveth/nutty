b1e04f6c07a22cc28c1a1e047c8e1d3a6053ac15
John Carveth <jlcarveth@gmail.com>
2023-04-06 13:40:42 -0400

    Initial unit testing file

---
+++ test.ts
@@ -0,0 +1,20 @@
+// Import and run the paste server
+import "./paste.ts";
+
+async function testVersion() {
+  const expectedVersion = "0.5.0";
+  const response = await fetch("http://localhost:5335/api/version");
+  const actualVersion = await response.text();
+  return expectedVersion === actualVersion;
+}
+let testsPassed = true;
+const testResults = {
+  "testVersion": await testVersion(),
+};
+console.log(
+  "Running test testVersion: ",
+  (testResults["testVersion"]) ? "Passed ✓" : "Failed ✗",
+);
+
+testsPassed = Object.values(testsPassed).every((value) => value === true);
+Deno.exit(testsPassed ? 0 : 1);
