2f6fbbea17de79d37c6cf9bf6f133d94e9450db8
John Carveth <jlcarveth@gmail.com>
2023-10-06 14:53:49 -0400

    Fix navbar brand link
    Signed-off-by: John Carveth <jlcarveth@gmail.com>

--- templates/navbar.ts
+++ templates/navbar.ts
@@ -3,7 +3,7 @@ import { LayoutData } from "./layout.ts"
 export function Navbar(data: LayoutData) {
   return `
     <div class="navbar">
-      <h1><a href="#">Paste.ts</a></h1>
+      <h1><a href="/">Paste.ts</a></h1>
       <p class="version">${data.version}</p>
     </div>
 `
