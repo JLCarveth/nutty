dd6cd5fc17e461e22eeba6bed3c92c9d7b28ba3d
John Carveth <jlcarveth@gmail.com>
2024-01-03 11:57:19 -0500

    Update copyright year
    Signed-off-by: John Carveth <jlcarveth@gmail.com>

--- templates/footer.ts
+++ templates/footer.ts
@@ -1,7 +1,7 @@
 export function Footer() {
   return `
 <footer>
-  <p>Copyright © 2023 John Lee Carveth</p>
+  <p>Copyright © 2024 John Lee Carveth</p>
   <ul class="footer-links">
     <li><a href="https://github.com/jlcarveth/nutty">GitHub</a></li>
   </ul>
